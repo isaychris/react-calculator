f127edb2b81eb47b562537ca704c00afb443f541
isaychris <adaptxsurvive@gmail.com>
2018-10-05 17:11:52 -0700

    fixed input logic; added comments

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Display from './components/display'
 import History from './components/history'
 import logo from './logo.svg';
 import './App.css'
-
 const math = require('mathjs')
 
 class App extends Component {
@@ -18,6 +17,7 @@ class App extends Component {
         }
     }
 
+    // callback for display component; handles and filters the changing input from keyboard.
     handleChange = (e) => {
         let char = e.target.value[e.target.value.length - 1]
         if(["*", "/", ".", "+", "-", "^", 
@@ -27,46 +27,60 @@ class App extends Component {
     }
 }
 
-    updateDisplayHistory = (new_exp,) => {
+    // callback for history component; updates the display when user clicks on history list.
+    updateDisplayHistory = (history_exp) => {
         this.setState({
-            input: new_exp
+            input: history_exp
         })
     }
 
+    // callback for display component; updates the display with new input
+    updateDisplay = (new_input) => {
+        let prev = this.state.input;
+
+        // if a result was computed
+        if(this.state.done) {
+            // check if new input is operator,
+            if(["*", "/", ".", "+", "-", "^"].includes(new_input)) {
+                // if previous input was an operator, dont do anything
+                if(prev[prev.length -1] === new_input) {
+                    return;
+                }
+                // if previous was a number, append input with operator
+                this.setState({
+                    input: this.state.input + new_input,
+                    done: false
+                })
+            // if new input is number, replace input with number.
+            } else {
+                this.setState({
+                    input: new_input,
+                    done: false
+                })
+            }
+        // else if result wasnt computed yet, append new input with input.
+        } else {
+            this.setState({
+                input: this.state.input + new_input,
+            })
+        }
+    }
+
+    //  adds expression and result to history state
     addToHistory = (new_exp, result) => {
         this.setState({
             history: [...this.state.history, {exp: new_exp, result: result}],
         })
     }
 
-    updateDisplay = (new_input) => {
-        let prev = this.state.input;
-
-        if(["*", "/", ".", "+", "-", "^"].includes(new_input)) {
-            if(prev[prev.length -1] === new_input) {
-                return;
-            }
-        }
-
-        if(this.state.done) {
-            this.setState({
-                input: new_input,
-                done: false
-            })
-
-        } else {
-            this.setState({
-                input: this.state.input + new_input
-            })
-        }
-    }
-
+    // clears the display by setting it to empty
     clearDisplay = () => {
         this.setState({
             input: ""
         })
     }
 
+    // calculates the expression and sends it to history
     calculate = () => {
         if(this.state.input !== "") {
             try {
@@ -85,6 +99,7 @@ class App extends Component {
             }
         }
     }
+
     render() {
         return (
             <div className="App container">
